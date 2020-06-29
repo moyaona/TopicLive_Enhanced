397e4e8cafedea1e868260b826f2fec65e21a768
Wolf Clement <c.wolf@kiwec.net>
2020-06-29 18:03:58 +0200

    HTTPS

--- topiclive.user.js
+++ topiclive.user.js
@@ -2,17 +2,17 @@
 // @name TopicLive
 // @description Charge les nouveaux messages d'un topic de JVC en direct
 // @author kiwec
-// @match http://www.jeuxvideo.com/*
-// @match http://m.jeuxvideo.com/*
+// @match https://www.jeuxvideo.com/*
+// @match https://m.jeuxvideo.com/*
 // @run-at document-end
-// @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @require Favicon.js
 // @require Formulaire.js
 // @require Message.js
 // @require Option.js
 // @require Page.js
 // @require TopicLive.js
-// @version 5.2.5
+// @version 5.2.6
 // @grant none
 // @noframes
 // ==/UserScript==
